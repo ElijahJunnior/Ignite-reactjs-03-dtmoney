6869a6e3727cdc0031fecfde8fa3cd514a48e2d0
Elias Junior <elijah.junnior@outlook.com>
2021-07-29 06:18:56 -0300

    developer of dashboard component finished

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
-import { Summary } from "../Summary";
 import { Container } from "./styles";
 
+import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionTable";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionTable />
         </Container>
     )
 }
