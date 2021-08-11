28bacfa72332c70f5d6fd8354f8e58630570bb12
Elias Junior <elijah.junnior@outlook.com>
2021-08-11 06:44:54 -0300

    Transaction Context Created and added in TransactionTable Component

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,25 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 
-import { Container } from './styles'
+import { TransactionsContext } from '../../TransactionsContext';
 
-import { api } from '../../services/api';
-
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
-}
+import { Container } from './styles';
 
 export function TransactionTable() {
 
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-    useEffect(() => {
-        api.get('transactions').then(response => setTransactions(response.data.transactions));
-    }, []);
+    const transactions = useContext(TransactionsContext);
 
     return (
 
